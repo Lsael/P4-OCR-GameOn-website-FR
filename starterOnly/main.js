fa3d15c66573c93f5dfdada0116892b79dceedd8
Lsael <l.saelens@proton.me>
2023-06-19 16:22:38 +0200

    Created validate function that trigger on form submission

--- starterOnly/main.js
+++ starterOnly/main.js
@@ -6,3 +6,15 @@ const editNav = () => {
       x.className = "topnav";
     }
   }
+
+const validate = () => {
+  const firstName = document.querySelector("#first").value
+  const lastName = document.querySelector("#last").value
+  const email = document.querySelector("#email").value
+  const birthdate = document.querySelector("#birthdate").value
+  const quantity = document.querySelector("#quantity").value
+  const cgValid = document.querySelector("#checkbox1").value
+  const newsLetterSubcribe = document.querySelector("#checkbox2").value
+
+  return false
+}
