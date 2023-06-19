0f08bcfbf932558396757da2accf0ba5fe7f9e5d
Lsael <l.saelens@proton.me>
2023-06-19 19:19:09 +0200

    Added function to test name with regex

--- starterOnly/main.js
+++ starterOnly/main.js
@@ -7,6 +7,13 @@ const editNav = () => {
     }
   }
 
+// validName verify if the name contain only alphabetic caracters, return true if valid
+const validName = (name) => {
+  const regex = /[^aA-zZ\s]/
+  return !regex.test(name)
+}
+
+// function is called on modal submit
 const validate = () => {
   const firstName = document.querySelector("#first").value
   const lastName = document.querySelector("#last").value
@@ -15,6 +22,8 @@ const validate = () => {
   const quantity = document.querySelector("#quantity").value
   const cgValid = document.querySelector("#checkbox1").value
   const newsLetterSubcribe = document.querySelector("#checkbox2").value
+
+  console.log(validName(firstName))
 
   return false
 }
