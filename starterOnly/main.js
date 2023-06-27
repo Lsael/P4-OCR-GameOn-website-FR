63e7da7a8292b21ab86d473171d8684a825b75d9
LSaelens <l.saelens@laposte.net>
2023-06-27 16:08:31 +0200

    Update

--- starterOnly/main.js
+++ starterOnly/main.js
@@ -18,7 +18,7 @@ const formValidationModal = () => {
 };
 
 // validName verify the name, return true if valid
-const validName = (name) => {
+const checkName = (name) => {
   const regex = /^[aA-zZ\s-]{2,}$/;
   return regex.test(name);
 };
@@ -36,70 +36,77 @@ const validBirthdate = (birthdate) => {
   return date instanceof Date && !isNaN(date) && date.getFullYear() >= 1900 && date <= currentDate;
 };
 
+
 // function is called on modal submit
 const validate = () => {
-  let status = true;
+  let isFormValidated = true;
+
   const firstname = document.querySelector('#first');
   const lastname = document.querySelector('#last');
   const email = document.querySelector('#email');
   const birthdate = document.querySelector('#birthdate');
   const quantity = document.querySelector('#quantity');
   const tournament = document.querySelectorAll('input[name="location"]:checked');
+  // modifier le nom
   const cgValid = document.querySelector('#checkbox1');
 
+
+  // factoriser le classlist add
   if (!validName(firstname.value)) {
     firstname.classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     firstname.classList.remove('error-display');
   }
 
   if (!validName(lastname.value)) {
     lastname.classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     lastname.classList.remove('error-display');
   }
 
   if (!validEmail(email.value)) {
     email.classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     email.classList.remove('error-display');
   }
 
   if (!validBirthdate(birthdate.value)) {
     birthdate.classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     birthdate.classList.remove('error-display');
   }
 
   if (!quantity.value || quantity.value < 1 || quantity.value > 99) {
     quantity.classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     quantity.classList.remove('error-display');
   }
   if (tournament.length === 0) {
     document.querySelector('#location1').classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     document.querySelector('#location1').classList.remove('error-display');
   }
+
   if (!cgValid.checked) {
     document.querySelector('#checkbox1').classList.add('error-display');
-    status = false;
+    isFormValidated = false;
   } else {
     document.querySelector('#checkbox1').classList.remove('error-display');
   }
 
-  if (!status) {
+  if (!isFormValidated) {
     document.querySelector('.btn-submit').classList.add('error-display');
   } else {
     document.querySelector('.btn-submit').classList.remove('error-display');
     formValidationModal();
   }
 
+  // deplacer dans le html
   return false;
 };
